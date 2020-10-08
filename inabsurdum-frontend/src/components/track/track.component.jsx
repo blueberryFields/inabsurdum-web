57f347d66c34f35447de8ba5686195af38a8e8e7
Sebastian Kihlstrand <sebastian.kihlstrand@hotmail.com>
2020-10-08 15:50:43 +0200

    Add fuctionality for pausing and then playing from icon in track-component.

--- inabsurdum-frontend/src/components/track/track.component.jsx
+++ inabsurdum-frontend/src/components/track/track.component.jsx
@@ -10,7 +10,7 @@ import {
 
 import './track.styles.scss';
 import { selectIsTrackSelected } from '../../redux/player/player.selectors';
-import { selectTrack } from '../../redux/player/player.actions';
+import { selectTrack, togglePlaying } from '../../redux/player/player.actions';
 
 const Track = ({ track }) => {
   const { title, length } = track;
@@ -18,12 +18,17 @@ const Track = ({ track }) => {
   const isTrack = useSelector(selectIsTrackSelected(track));
   const dispatch = useDispatch();
 
+  const handlePlayPause = () => {
+    isTrack.playing
+      ? dispatch(togglePlaying())
+      : isTrack.selected
+      ? dispatch(togglePlaying())
+      : dispatch(selectTrack(track));
+  };
+
   return (
     <tr className={`${isTrack.selected ? 'selected' : ''} track`}>
-      <td
-        onClick={() => dispatch(selectTrack(track))}
-        className="play"
-      >
+      <td onClick={handlePlayPause} className="play">
         <FontAwesomeIcon
           className="play-icon"
           icon={isTrack.playing ? faPause : faPlay}
