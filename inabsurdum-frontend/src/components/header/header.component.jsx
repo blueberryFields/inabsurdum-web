92ab39d5e2c1059a09c9beb462eb5fdcb211e0ec
Sebastian Kihlstrand <sebastian.kihlstrand@hotmail.com>
2020-10-08 18:09:58 +0200

    Hide upload and signout buttons when not signed in.

--- inabsurdum-frontend/src/components/header/header.component.jsx
+++ inabsurdum-frontend/src/components/header/header.component.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { signOut } from '../../redux/user/user.actions';
 import CustomButton from '../custom-button/custom-button.component';
 import ToggleContent from '../../modal-engine/toggle-content/toggle-content.component';
 import Modal from '../../modal-engine/modal/modal.component';
 import UploadModal from '../upload-modal/upload-modal.component';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import './header.styles.scss';
 
 const Header = () => {
   const dispatch = useDispatch();
+  const user = useSelector(selectCurrentUser);
 
   return (
     <div className="header">
-      <ToggleContent
-        toggle={(show) => <CustomButton onClick={show}>Ladda upp</CustomButton>}
-        content={(hide) => (
-          <Modal>
-            <UploadModal hide={hide} />
-          </Modal>
-        )}
-      />
+      {user && (
+        <ToggleContent
+          toggle={(show) => (
+            <CustomButton onClick={show}>Ladda upp</CustomButton>
+          )}
+          content={(hide) => (
+            <Modal>
+              <UploadModal hide={hide} />
+            </Modal>
+          )}
+        />
+      )}
       <h1 className="logo">In Absurdum</h1>
       {/* <CustomButton inverted>Logga ut</CustomButton> */}
       {/* <FontAwesomeIcon className="menu-icon" icon={faBars} /> */}
-      <div className="sign-out" onClick={() => dispatch(signOut())}>
-        LOGGA UT
-      </div>
+      {user && (
+        <div className="sign-out" onClick={() => dispatch(signOut())}>
+          LOGGA UT
+        </div>
+      )}
     </div>
   );
 };
